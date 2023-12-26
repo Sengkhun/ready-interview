72220dd2c68bc4334c0c817e5865185f62ca4575
Lim Sengkhun <limsengkhun@gmail.com>
2023-12-26 17:26:59 +1030

    setup database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { getXataClient } from "@/xata";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const xataClient = getXataClient();
+  const blogs = await xataClient.db.blogs.getMany();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UserButton afterSignOutUrl="/" />
       <div>Hello</div>
+      {blogs.map((blog) => (
+        <p key={blog.id}>{blog.name}</p>
+      ))}
     </main>
   );
 }
