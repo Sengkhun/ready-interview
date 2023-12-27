0e517a0954ae220b90f318308bf56da6c18dd25f
Lim Sengkhun <limsengkhun@gmail.com>
2023-12-27 23:20:10 +1030

    add drawer menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import { getXataClient } from "@/xata";
 
 import Navbar from "@/components/Navbar";
 
+// Configure Font Awesome
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 const poppins = Poppins({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
@@ -13,9 +18,7 @@ export default async function Home() {
   // const xataClient = getXataClient();
   // const blogs = await xataClient.db.blogs.getMany();
   return (
-    <main
-      className={`${poppins.variable} font-sans container mx-auto *:px-3 lg:*:px-0`}
-    >
+    <main className={`${poppins.variable} font-sans *:px-3 lg:*:px-0`}>
       <Navbar />
     </main>
   );
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,75 +1,166 @@
 "use client";
+import { useState } from "react";
 import { UserButton, useUser } from "@clerk/nextjs";
 
 import Image from "next/image";
 import Link from "next/link";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faHouse,
+  faBuilding,
+  faCircleInfo,
+  faBlog,
+} from "@fortawesome/free-solid-svg-icons";
+
 import constant from "@/constant";
-import Button from "../Buttons/Button";
+import Button from "@/components/Buttons/Button";
 
 const MENU = [
-  { id: 1, title: "Company", path: "/company" },
-  { id: 2, title: "About Us", path: "/about-us" },
-  { id: 3, title: "Blog", path: "/blog" },
+  { title: "Company", icon: faBuilding, path: "/company" },
+  { title: "About Us", icon: faCircleInfo, path: "/about-us" },
+  { title: "Blog", icon: faBlog, path: "/blog" },
 ];
 
+const DRAWER_MENU = [{ title: "Home", icon: faHouse, path: "/" }, ...MENU];
+
 export default function Navbar() {
+  // Hooks
   const { user, isLoaded } = useUser();
 
+  // States
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const openDrawer = () => setDrawerOpen(true);
+  const closeDrawer = () => setDrawerOpen(false);
+
   return (
-    <div className="flex flex-row justify-between items-center pt-3">
-      {/* Logo */}
-      <Link href="/" title={`${constant.name} - Home`}>
-        {/* Small Logo */}
-        <Image
-          className="h-auto lg:hidden"
-          src="/images/logos/logo.svg"
-          width={50}
-          height={50}
-          alt={`${constant.name} Logo`}
-        />
+    <>
+      <div className="sticky top-0 shadow-md bg-white">
+        <div className="container mx-auto flex flex-row justify-between items-center py-3 lg:py-5">
+          {/* Logo */}
+          <Link
+            href="/"
+            title={`${constant.name} - Home`}
+            className="w-14 lg:w-28"
+          >
+            {/* Small Logo */}
+            <Image
+              className="lg:hidden"
+              src="/images/logos/logo.svg"
+              width={50}
+              height={50}
+              alt={`${constant.name} Logo`}
+            />
 
-        {/* Full Logo */}
-        <Image
-          className="hidden lg:flex h-auto"
-          src="/images/logos/logo-full.svg"
-          width={120}
-          height={100}
-          alt={`${constant.name} Logo`}
-        />
-      </Link>
+            {/* Full Logo */}
+            <Image
+              className="hidden lg:flex"
+              src="/images/logos/logo-full.svg"
+              width={120}
+              height={100}
+              alt={`${constant.name} Logo`}
+            />
+          </Link>
 
-      {/* Menu */}
-      <ul className="hidden lg:flex flex-row">
-        {MENU.map((item) => (
-          <li key={item.id}>
-            <Link
-              href={item.path}
-              title={`${constant.name} - ${item.title}`}
-              className="block px-8 py-4"
+          {/* Menu */}
+          <ul className="hidden lg:flex flex-row">
+            {MENU.map((item, idx) => (
+              <li key={idx}>
+                <Link
+                  href={item.path}
+                  title={`${constant.name} - ${item.title}`}
+                  className="block px-12 py-4"
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          {/* Action Button */}
+          <div className="lg:min-w-20 flex flex-row justify-end items-center ml-4">
+            {isLoaded ? (
+              user ? (
+                <UserButton afterSignOutUrl="/" />
+              ) : (
+                <Button
+                  href="/sign-in"
+                  title={`${constant.name} - Sign In`}
+                  className="btn btn-primary"
+                >
+                  Sign In
+                </Button>
+              )
+            ) : null}
+
+            {/* Menu */}
+            <button
+              className="lg:hidden flex flex-row justify-end items-center w-12"
+              onClick={openDrawer}
             >
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      {/* Action Button */}
-      <div className="lg:min-w-20 flex flex-row justify-end">
-        {isLoaded ? (
-          user ? (
-            <UserButton afterSignOutUrl="/" />
-          ) : (
-            <Button
-              href="/sign-in"
-              title={`${constant.name} - Sign In`}
-              className="btn btn-primary"
-            >
-              Sign In
-            </Button>
-          )
-        ) : null}
+              <FontAwesomeIcon
+                className={`fa-xl transition-all${drawerOpen ? " w-0" : ""}`}
+                icon={faBars}
+              />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+
+      {/* Drawer Menu */}
+      <div className="flex lg:hidden">
+        <div
+          className={`${
+            drawerOpen ? "translate-x-0 " : ""
+          }fixed top-0 left-0 z-20 w-64 h-full transition-all duration-500 transform -translate-x-full bg-white shadow-lg`}
+          style={{ padding: 0 }}
+        >
+          {/* Logo */}
+          <Link
+            href="/"
+            title={`${constant.name} - Home`}
+            className="block border-b px-6 py-8"
+          >
+            <Image
+              src="/images/logos/logo-full.svg"
+              width={120}
+              height={100}
+              alt={`${constant.name} Logo`}
+            />
+          </Link>
+
+          <ul className="flex flex-col">
+            {DRAWER_MENU.map((item, idx) => (
+              <li
+                key={idx}
+                className="hover:bg-primary hover:text-white transition-all"
+              >
+                <Link
+                  href={item.path}
+                  title={`${constant.name} - ${item.title}`}
+                  className="block hover:color-red w-full pr-8 py-4"
+                >
+                  <FontAwesomeIcon
+                    className="fa-xl flex flex-row justify-start items-center w-20"
+                    icon={item.icon}
+                  />
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Background Overlay */}
+        <div
+          className={`fixed top-0 left-0 right-0 h-full w-full transition-all duration-500${
+            drawerOpen ? " bg-black opacity-90" : " -z-10 opacity-0"
+          }`}
+          onClick={closeDrawer}
+        ></div>
+      </div>
+    </>
   );
 }
